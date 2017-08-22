63167794308277b8adb04412d7f99448e48c3d8a
yblew <wobeng@yblew.com>
2017-08-22 18:18:12 -0400

    started working on adding cluster

--- upload_lambda.sh
+++ upload_lambda.sh
@@ -2,25 +2,30 @@
 
 set -e
 
-REGION="ap-southeast-2"
-
+if [ -z "$REGION" ]; then
+    echo "Please define the REGION variable before executing this script."
+    exit -1
+fi
 
 if [ -z "$BUCKET" ]; then
     echo "Please define the BUCKET variable before executing this script."
     exit -1
 fi
-
+if [ -z "$PROFILE" ]; then
+    echo "Please define the PROFILE variable before executing this script."
+    exit -1
+fi
 
 # Install dependancies locally, so they get included in the zip file
 pip install -r requirements.txt -t lambda
 
-aws cloudformation package \
+aws cloudformation package --profile ${PROFILE}\
   --template-file cloudformation.yaml   \
   --output-template-file generated-cloudformation.yaml    \
   --s3-bucket ${BUCKET}
 
 
-aws cloudformation deploy \
+aws cloudformation deploy --profile ${PROFILE}\
   --template-file generated-cloudformation.yaml \
   --stack-name "aws-backup-lambda" \
   --region ${REGION} \
